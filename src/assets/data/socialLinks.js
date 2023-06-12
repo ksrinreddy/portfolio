c072b25bd57603994ea03671528c0d04c8f0a99b
ksrinreddy <ksrinreddy@gmail.com>
2023-06-12 19:20:38 +0530

    add social links data to assets

---
+++ src/assets/data/socialLinks.js
@@ -0,0 +1,16 @@
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+
+const socialLinks = [
+  {
+    id: 1,
+    url: "https://github.com/ksrinreddy",
+    icon: <AiFillGithub />,
+  },
+  {
+    id: 2,
+    url: "https://www.linkedin.com/in/ksrinreddy",
+    icon: <AiFillLinkedin />,
+  },
+];
+
+export default socialLinks;
