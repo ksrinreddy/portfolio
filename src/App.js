1f5bb42e2e56983b267ddf299eee4703c3322385
ksrinreddy <ksrinreddy@gmail.com>
2023-06-12 19:36:50 +0530

    update structure of app, setup basic routing of app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-
+import Sidebar from "./components/Sidebar";
+import { useGlobalContext } from "./context";
 import Error from "./pages/Error";
 import Home from "./pages/Home";
+import Skills from "./pages/Skills";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import ScrollTopBtn from "./components/ScrollTopBtn";
 
 const App = () => {
+  const { isSidebarOpen, scrollHeight } = useGlobalContext();
+
   return (
     <BrowserRouter>
       <Navbar />
+      <Sidebar />
+
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
+      {!isSidebarOpen && scrollHeight > 500 && <ScrollTopBtn />}
     </BrowserRouter>
   );
 };
