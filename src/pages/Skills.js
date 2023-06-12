e3924792c3002c4c6efbb759e1f5d32915a5d49e
ksrinreddy <ksrinreddy@gmail.com>
2023-06-12 19:54:38 +0530

    create, initial style skills page

---
+++ src/pages/Skills.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Skills = () => {
+  return (
+    <section className="skills">
+      <h2 className="skills__title">skills</h2>
+      <div className="skills__wrapper">skills content</div>
+    </section>
+  );
+};
+
+export default Skills;
