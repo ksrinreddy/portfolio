8fb1bdfd33f730015bd396213a138a46d42b0692
ksrinreddy <ksrinreddy@gmail.com>
2023-06-12 19:19:36 +0530

    add navLinks data to assets

---
+++ src/assets/data/navLinks.js
@@ -0,0 +1,42 @@
+import {
+  AiOutlineHome,
+  AiOutlineInfoCircle,
+  AiOutlineProject,
+} from "react-icons/ai";
+import { GiSkills } from "react-icons/gi";
+import { MdOutlineContactPage } from "react-icons/md";
+
+const navLinks = [
+  {
+    id: 1,
+    url: "/",
+    text: "home",
+    icon: <AiOutlineHome />,
+  },
+  {
+    id: 2,
+    url: "/projects",
+    text: "projects",
+    icon: <AiOutlineProject />,
+  },
+  {
+    id: 3,
+    url: "/skills",
+    text: "skills",
+    icon: <GiSkills />,
+  },
+  {
+    id: 4,
+    url: "/about",
+    text: "about",
+    icon: <AiOutlineInfoCircle />,
+  },
+  {
+    id: 5,
+    url: "/contact",
+    text: "contact",
+    icon: <MdOutlineContactPage />,
+  },
+];
+
+export default navLinks;
