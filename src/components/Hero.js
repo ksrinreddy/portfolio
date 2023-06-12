4df6676206b6570f287a27f6ca08f47f0675a236
ksrinreddy <ksrinreddy@gmail.com>
2023-06-12 20:06:15 +0530

    structure and style hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import socialLinks from "../assets/data/socialLinks";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="hero">
+      <div className="hero__content-wrapper">
+        <div className="hero__info">
+          <div className="hero__header">
+            <h3 className="hero__name">ksrin reddy</h3>
+          </div>
+          <h1 className="hero__role-title">
+            Frontend <br /> React Developer
+          </h1>
+          <p className="hero__role-desc">
+            Build simple and efficient user interfaces
+          </p>
+          <Link to="/projects" className="hero__projects-link">
+            Projects
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
