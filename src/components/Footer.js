728013e1b79efa842f848088f638f70bce9d5080
ksrinreddy <ksrinreddy@gmail.com>
2023-06-12 20:07:10 +0530

    structure and style footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
+
+import socialLinks from "../assets/data/socialLinks";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const [year, setYear] = useState(null);
+  useEffect(() => {
+    setYear(new Date().getFullYear());
+  }, []);
+
+  return (
+    <footer className="footer">
+      <div className="footer__content-wrapper">
+        <p className="footer__logo">ksrinreddy</p>
+        <div className="footer__social-links">
+          {socialLinks.map((socialLink) => {
+            const { id, url, icon } = socialLink;
+            return (
+              <a
+                href={url}
+                target="_blank"
+                rel="noreferrer"
+                key={id}
+                className="footer__social-link"
+              >
+                {icon}
+              </a>
+            );
+          })}
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
