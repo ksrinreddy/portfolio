8c370bf12f76ce8a0f0ea3d69d7c9b2fe134a272
ksrinreddy <ksrinreddy@gmail.com>
2023-06-12 20:02:33 +0530

    structured and styled navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
+import navLinks from "../assets/data/navLinks";
+import { useGlobalContext } from "../context";
+import resumePdf from "../assets/data/resume.pdf";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const { setNavbarHeight, navbarRef, scrollHeight, toggleSidebar } =
+    useGlobalContext();
+
+  useEffect(() => {
+    const navHeight = navbarRef.current.getBoundingClientRect().height;
+    setNavbarHeight(navHeight);
+  }, [scrollHeight]);
+
+  return (
+    <nav className="nav" ref={navbarRef}>
+      <div className="nav__center">
+        <div className="nav__header">
+          <Link to="/" className="nav__logo">
+            ksrinreddy
+          </Link>
+          <div className="nav__links">
+            {navLinks.map((navLink) => {
+              const { id, url, text } = navLink;
+              return (
+                <NavLink key={id} to={`${url}`} className="nav__link">
+                  {text}
+                </NavLink>
+              );
+            })}
+            <a
+              href={resumePdf}
+              target="_blank"
+              rel="noreferrer"
+              className="nav__resume-link"
+            >
+              Resume
+            </a>
+          </div>
+          <button
+            type="button"
+            className="nav__toggle-btn"
+            onClick={toggleSidebar}
+          >
+            <FaBars className="nav__toggle-icon" />
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
