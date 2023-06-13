4447688d930d86ebd30ee3bbbb1bb96db2d6784f
ksrinreddy <ksrinreddy@gmail.com>
2023-06-13 18:26:34 +0530

    create projects.js, add project specific data to render each project seperately

---
+++ src/assets/data/projects.js
@@ -0,0 +1,235 @@
+// project background images
+import netflixBg from "../images/netflix-bg.png";
+import portfolioBg from "../images/portfolio-bg.png";
+import resumeBg from "../images/resume-bg.png";
+// langugaes images
+import htmlLogo from "../images/html-logo.png";
+import cssLogo from "../images/css-logo.png";
+import javascriptLogo from "../images/javascript-logo.png";
+import sassLogo from "../images/sass-logo.png";
+import styledComponentsLogo from "../images/styled-components-logo.png";
+// libraries images
+import reactLogo from "../images/react-logo.png";
+import reduxToolkitLogo from "../images/redux-toolkit-logo.png";
+import reactRouterLogo from "../images/react-router-logo.png";
+// version control systems imgaes
+import gitLogo from "../images/git-logo.png";
+// internet hosting services images
+import gitHubLogo from "../images/github-logo.png";
+// deploy platforms images
+import netlifyLogo from "../images/netlify-logo.png";
+// react icons
+import { DiGithubBadge } from "react-icons/di";
+import { HiExternalLink } from "react-icons/hi";
+
+// resume
+import resumePdf from "./resume.pdf";
+
+// netflix
+export const netflixBuildTools = [
+  {
+    id: 1,
+    name: "HTML",
+    img: { htmlLogo },
+    url: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+  },
+  {
+    id: 2,
+    name: "CSS",
+    img: { cssLogo },
+    url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+  },
+  {
+    id: 3,
+    name: "JavaScript",
+    img: { javascriptLogo },
+    url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+  },
+  {
+    id: 4,
+    name: "react",
+    img: reactLogo,
+    url: "https://legacy.reactjs.org/",
+  },
+  {
+    id: 5,
+    name: "react toolkit",
+    img: reduxToolkitLogo,
+    url: "https://redux-toolkit.js.org/",
+  },
+  {
+    id: 6,
+    name: "react router",
+    img: reactRouterLogo,
+    url: "https://reactrouter.com/en/main",
+  },
+  {
+    id: 7,
+    name: "sass",
+    img: sassLogo,
+    url: "https://sass-lang.com/",
+  },
+  {
+    id: 8,
+    name: "git",
+    img: gitLogo,
+    url: "https://git-scm.com/",
+  },
+  {
+    id: 9,
+    name: "GitHub",
+    img: gitHubLogo,
+    url: "https://github.com/",
+  },
+  {
+    id: 10,
+    name: "netilfy",
+    img: netlifyLogo,
+    url: "https://www.netlify.com/",
+  },
+];
+
+export const netflixBuildLinks = [
+  {
+    id: 1,
+    url: "https://netflix-clone-react-reduxtoolkit.netlify.app",
+    icon: <HiExternalLink />,
+  },
+  {
+    id: 2,
+    url: "https://github.com/ksrinreddy/netflix-clone-react-reduxtoolkit",
+    icon: <DiGithubBadge />,
+  },
+];
+
+export const netflixBuildBg = netflixBg;
+
+// portfolio
+export const portfolioBuildTools = [
+  {
+    id: 1,
+    name: "HTML",
+    img: { htmlLogo },
+    url: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+  },
+  {
+    id: 2,
+    name: "CSS",
+    img: { cssLogo },
+    url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+  },
+  {
+    id: 3,
+    name: "JavaScript",
+    img: { javascriptLogo },
+    url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+  },
+  {
+    id: 4,
+    name: "react",
+    img: reactLogo,
+    url: "https://legacy.reactjs.org/",
+  },
+  {
+    id: 5,
+    name: "react router",
+    img: reactRouterLogo,
+    url: "https://reactrouter.com/en/main",
+  },
+  {
+    id: 6,
+    name: "sass",
+    img: sassLogo,
+    url: "https://sass-lang.com/",
+  },
+  {
+    id: 7,
+    name: "git",
+    img: gitLogo,
+    url: "https://git-scm.com/",
+  },
+  {
+    id: 8,
+    name: "GitHub",
+    img: gitHubLogo,
+    url: "https://github.com/",
+  },
+  {
+    id: 9,
+    name: "netilfy",
+    img: netlifyLogo,
+    url: "https://www.netlify.com/",
+  },
+];
+
+export const portfolioBuildLinks = [
+  {
+    id: 1,
+    url: "https://portfolio-ksrinreddy.netlify.app",
+    icon: <HiExternalLink />,
+  },
+  {
+    id: 2,
+    url: "https://github.com/ksrinreddy/portfoilio",
+    icon: <DiGithubBadge />,
+  },
+];
+
+// resume
+export const resumeBuildTools = [
+  {
+    id: 1,
+    name: "HTML",
+    img: { htmlLogo },
+    url: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+  },
+  {
+    id: 2,
+    name: "CSS",
+    img: { cssLogo },
+    url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+  },
+  {
+    id: 3,
+    name: "JavaScript",
+    img: { javascriptLogo },
+    url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+  },
+  {
+    id: 4,
+    name: "react",
+    img: reactLogo,
+    url: "https://legacy.reactjs.org/",
+  },
+  {
+    id: 5,
+    name: "styled components",
+    img: styledComponentsLogo,
+    url: "https://styled-components.com/",
+  },
+  {
+    id: 6,
+    name: "git",
+    img: gitLogo,
+    url: "https://git-scm.com/",
+  },
+  {
+    id: 7,
+    name: "GitHub",
+    img: gitHubLogo,
+    url: "https://github.com/",
+  },
+];
+
+export const resumeBuildLinks = [
+  {
+    id: 1,
+    url: resumePdf,
+    icon: <HiExternalLink />,
+  },
+  {
+    id: 2,
+    url: "https://github.com/ksrinreddy/resume-entry-level",
+    icon: <DiGithubBadge />,
+  },
+];
