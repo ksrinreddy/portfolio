94298bf3a2f2406c44c9adae15e0809044657a15
ksrinreddy <ksrinreddy@gmail.com>
2023-06-13 16:20:08 +0530

    remove structured and styled ProjectsPreview, ProjectPreview components as they are not needed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,10 @@
 import React from "react";
 import Hero from "../components/Hero";
-import ProjectsPreview from "../components/ProjectsPreview";
-import { useGlobalContext } from "../context";
 
 const Home = () => {
   return (
     <main className="home">
       <Hero />
-      {/* <ProjectsPreview /> */}
     </main>
   );
 };
