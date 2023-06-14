6e1a100978dd73e330a2913452fb455a68b3f460
ksrinreddy <ksrinreddy@gmail.com>
2023-06-14 14:51:44 +0530

    structure netflix project's card, links, tools and info

--- src/components/NetflixClone.js
+++ src/components/NetflixClone.js
@@ -1,7 +1,100 @@
 import React from "react";
+import {
+  netflixBuildBg,
+  netflixBuildLinks,
+  netflixBuildTools,
+} from "../assets/data/projects";
 
 const NetflixClone = () => {
-  return <div>NetflixClone</div>;
+  return (
+    <div className="project">
+      {/* card */}
+      <div className="project__card">
+        <img src={netflixBuildBg} alt="project logo" className="project__img" />
+        <div className="project__card-overlay">
+          <div className="project__web-links">
+            {netflixBuildLinks?.map((link) => {
+              const { id, url, icon } = link;
+              return (
+                <a
+                  href={url}
+                  key={id}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="project__web-link"
+                >
+                  {icon}
+                </a>
+              );
+            })}
+          </div>
+        </div>
+        <h4 className="project__title">Netflix Clone</h4>
+      </div>
+      {/* info */}
+      <div className="project__info">
+        <div className="project__info-wrapper">
+          {/* tools */}
+          <div className="project__info-tools">
+            <span className="project__info-heading">Tools</span>
+            <div className="project__tools-wrapper">
+              {netflixBuildTools?.map((tool) => {
+                const { id, name, img, url } = tool;
+                return (
+                  <div key={id} className="project__tool">
+                    <a
+                      href={url}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="project__tool-link"
+                    >
+                      <img
+                        src={img}
+                        alt={name}
+                        className="project__tool-logo"
+                      />
+                    </a>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          {/* project desc */}
+          <div className="project__desc">
+            <span className="project__info-heading">Info</span>
+            <div className="project__desc-text-wrapper">
+              <p className="project__desc-text">
+                Cloned frontend build of Netflix app as Single Page Application
+                using React.
+              </p>
+              <p className="project__desc-text">
+                Build includes Landing page, SignUp page, SignIn page, User
+                page, Protected Route and other features.
+              </p>
+              <p className="project__desc-text">
+                Setup navigation within and outside the app with the help of
+                React-Router-v6.
+              </p>
+              <p className="project__desc-text">
+                Managed client state and server states using ReduxToolkit.
+              </p>
+              <p className="project__desc-text">
+                Worked with RESTfulAPIs and Axios to fetch movies data.
+              </p>
+              <p className="project__desc-text">
+                Styled app with the help of Sass.
+              </p>
+              <p className="project__desc-text">
+                Utilized Git for managing version control and GitHub for web
+                hosting codebase.
+              </p>
+              <p className="project__desc-text">Deployed app using Netlify.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NetflixClone;
