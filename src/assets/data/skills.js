8333f4513abb360a477e7cce611a607ef09a3479
ksrinreddy <ksrinreddy@gmail.com>
2023-06-15 14:41:31 +0530

    create skills.js data array, add names, logo's, urls to render through map

---
+++ src/assets/data/skills.js
@@ -0,0 +1,76 @@
+// langugaes images
+import htmlLogo from "../images/html-logo.png";
+import cssLogo from "../images/css-logo.png";
+import javascriptLogo from "../images/javascript-logo.png";
+import sassLogo from "../images/sass-logo.png";
+import styledComponentsLogo from "../images/styled-components-logo.png";
+// libraries images
+import reactLogo from "../images/react-logo.png";
+import reduxToolkitLogo from "../images/redux-toolkit-logo.png";
+import reactRouterLogo from "../images/react-router-logo.png";
+import reactQueryLogo from "../images/react-query-logo.png";
+// version control systems imgaes
+import gitLogo from "../images/git-logo.png";
+
+export const skills = [
+  {
+    id: 1,
+    name: "HTML",
+    image: htmlLogo,
+    url: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+  },
+  {
+    id: 2,
+    name: "CSS",
+    image: cssLogo,
+    url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+  },
+  {
+    id: 3,
+    name: "JavaScript",
+    image: javascriptLogo,
+    url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+  },
+  {
+    id: 4,
+    name: "ReactJS",
+    image: reactLogo,
+    url: "https://legacy.reactjs.org/",
+  },
+  {
+    id: 5,
+    name: "ReduxToolkit",
+    image: reduxToolkitLogo,
+    url: "https://redux-toolkit.js.org/",
+  },
+  {
+    id: 6,
+    name: "React Query",
+    image: reactQueryLogo,
+    url: "https://tanstack.com/query/v3/",
+  },
+  {
+    id: 7,
+    name: "React Router",
+    image: reactRouterLogo,
+    url: "https://reactrouter.com/en/main",
+  },
+  {
+    id: 8,
+    name: "Sass",
+    image: sassLogo,
+    url: "https://sass-lang.com/",
+  },
+  {
+    id: 9,
+    name: "Styled Components",
+    image: styledComponentsLogo,
+    url: "https://styled-components.com/",
+  },
+  {
+    id: 10,
+    name: "Git",
+    image: gitLogo,
+    url: "https://git-scm.com/",
+  },
+];
