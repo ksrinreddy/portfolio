d72332f6ebf052b39727f4c513385c4255590fff
ksrinreddy <ksrinreddy@gmail.com>
2023-06-16 19:50:36 +0530

    re-order social links data

--- src/assets/data/socialLinks.js
+++ src/assets/data/socialLinks.js
@@ -3,13 +3,13 @@ import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 const socialLinks = [
   {
     id: 1,
-    url: "https://github.com/ksrinreddy",
-    icon: <AiFillGithub />,
+    url: "https://www.linkedin.com/in/ksrinreddy",
+    icon: <AiFillLinkedin />,
   },
   {
     id: 2,
-    url: "https://www.linkedin.com/in/ksrinreddy",
-    icon: <AiFillLinkedin />,
+    url: "https://github.com/ksrinreddy",
+    icon: <AiFillGithub />,
   },
 ];
 
