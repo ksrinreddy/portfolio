c4a52f770cc5a898e92eaeeb82532535939b80c9
ksrinreddy <ksrinreddy@gmail.com>
2023-06-17 17:56:34 +0530

    structure error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Error = () => {
-  return <div>Error</div>;
+  console.log("there was an error");
+  return (
+    <section className="error">
+      <div className="error__wrapper">
+        <p className="error__message">
+          Error <span>404</span>. Page not found.
+        </p>
+
+        <Link to="/" className="error__home-link-btn">
+          Go Home
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default Error;
