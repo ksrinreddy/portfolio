f45cb3fd6e1033dd449a58d94a5eb5653bf07a3c
ksrinreddy <ksrinreddy@gmail.com>
2023-06-17 22:58:33 +0530

    update error message

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -2,14 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Error = () => {
-  console.log("there was an error");
   return (
     <section className="error">
       <div className="error__wrapper">
-        <p className="error__message">
-          Error <span>404</span>. Page not found.
-        </p>
-
+        <p className="error__message">Page not found.</p>
         <Link to="/" className="error__home-link-btn">
           Go Home
         </Link>
