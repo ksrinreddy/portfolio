87c9cb6affb66b00094396c7e172fdf0e0e30422
ksrinreddy <ksrinreddy@gmail.com>
2023-06-19 13:43:43 +0530

    update resume structure, replace bg image with text div

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,16 +1,12 @@
 import React from "react";
-import {
-  resumeBuildBg,
-  resumeBuildLinks,
-  resumeBuildTools,
-} from "../assets/data/projects";
+import { resumeBuildLinks } from "../assets/data/projects";
 
 const Resume = () => {
   return (
     <div className="project no-border-bottom">
       {/* card */}
       <div className="project__card">
-        <img src={resumeBuildBg} alt="project logo" className="project__img" />
+        <div className="project__title-card">Resume</div>
         <div className="project__card-overlay">
           <div className="project__web-links">
             {resumeBuildLinks.map((link) => {
@@ -29,40 +25,14 @@ const Resume = () => {
             })}
           </div>
         </div>
-        <h4 className="project__title">Resume</h4>
+        <h4 className="project__title-text">Resume</h4>
       </div>
       {/* info */}
       <div className="project__info">
         <div className="project__info-wrapper">
-          {/* tools */}
-          <div className="project__info-tools">
-            <span className="project__info-heading">Tools</span>
-            <div className="project__tools-wrapper">
-              {resumeBuildTools.map((tool) => {
-                const { id, name, img, url } = tool;
-                return (
-                  <div key={id} className="project__tool">
-                    <a
-                      href={url}
-                      target="_blank"
-                      rel="noreferrer"
-                      className="project__tool-link"
-                    >
-                      <img
-                        src={img}
-                        alt={name}
-                        className="project__tool-logo"
-                      />
-                    </a>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-          {/* project desc */}
           <div className="project__desc">
             <span className="project__info-heading">Info</span>
-            <div className="project__desc-text-wrapper">
+            <div className="project__info-text-wrapper">
               <p>
                 Designed conceptually and developed using React, out of
                 curiosity.
