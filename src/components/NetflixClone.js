233ab649c3b2748d67baa5dcd2d84b1d453aee5f
ksrinreddy <ksrinreddy@gmail.com>
2023-06-19 13:53:29 +0530

    update project structure and styles (remove build tools, bg-imgs)

--- src/components/NetflixClone.js
+++ src/components/NetflixClone.js
@@ -1,16 +1,12 @@
 import React from "react";
-import {
-  netflixBuildBg,
-  netflixBuildLinks,
-  netflixBuildTools,
-} from "../assets/data/projects";
+import { netflixBuildLinks } from "../assets/data/projects";
 
 const NetflixClone = () => {
   return (
     <div className="project">
       {/* card */}
       <div className="project__card">
-        <img src={netflixBuildBg} alt="project logo" className="project__img" />
+        <div className="project__title-card text-red">NETFLIX</div>
         <div className="project__card-overlay">
           <div className="project__web-links">
             {netflixBuildLinks.map((link) => {
@@ -29,61 +25,33 @@ const NetflixClone = () => {
             })}
           </div>
         </div>
-        <h4 className="project__title">Netflix Clone</h4>
+        <h4 className="project__title-text">Netflix Clone</h4>
       </div>
       {/* info */}
       <div className="project__info">
         <div className="project__info-wrapper">
-          {/* tools */}
-          <div className="project__info-tools">
-            <span className="project__info-heading">Tools</span>
-            <div className="project__tools-wrapper">
-              {netflixBuildTools.map((tool) => {
-                const { id, name, img, url } = tool;
-                return (
-                  <div key={id} className="project__tool">
-                    <a
-                      href={url}
-                      target="_blank"
-                      rel="noreferrer"
-                      className="project__tool-link"
-                    >
-                      <img
-                        src={img}
-                        alt={name}
-                        className="project__tool-logo"
-                      />
-                    </a>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-          {/* project desc */}
-          <div className="project__desc">
-            <span className="project__info-heading">Info</span>
-            <div className="project__desc-text-wrapper">
-              <p>
-                Cloned frontend build of Netflix app as Single Page Application
-                using React.
-              </p>
-              <p>
-                Build includes Landing page, SignUp page, SignIn page, User
-                page, Protected Route and other features.
-              </p>
-              <p>
-                Setup navigation within and outside the app with the help of
-                React-Router-v6.
-              </p>
-              <p>Managed client state and server states using ReduxToolkit.</p>
-              <p>Worked with RESTfulAPIs and Axios to fetch movies data.</p>
-              <p>Styled app with the help of Sass.</p>
-              <p>
-                Utilized Git for managing version control and GitHub for web
-                hosting codebase.
-              </p>
-              <p>Deployed app using Netlify.</p>
-            </div>
+          <span className="project__info-heading">Info</span>
+          <div className="project__info-text-wrapper">
+            <p>
+              Cloned frontend build of Netflix app as Single Page Application
+              using React.
+            </p>
+            <p>
+              Build includes Landing page, SignUp page, SignIn page, User page,
+              Protected Route and other features.
+            </p>
+            <p>
+              Setup navigation within and outside the app with the help of
+              React-Router-v6.
+            </p>
+            <p>Managed client state and server states using ReduxToolkit.</p>
+            <p>Worked with RESTfulAPIs and Axios to fetch movies data.</p>
+            <p>Styled app with the help of Sass.</p>
+            <p>
+              Utilized Git for managing version control and GitHub for web
+              hosting codebase.
+            </p>
+            <p>Deployed app using Netlify.</p>
           </div>
         </div>
       </div>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,20 +1,13 @@
 import React from "react";
-import {
-  portfolioBuildBg,
-  portfolioBuildLinks,
-  portfolioBuildTools,
-} from "../assets/data/projects";
+import { portfolioBuildLinks } from "../assets/data/projects";
 
 const Portfolio = () => {
   return (
-    <div className="project">
+    <div className="project no-border-bottom">
       {/* card */}
       <div className="project__card">
-        <img
-          src={portfolioBuildBg}
-          alt="project logo"
-          className="project__img"
-        />
+        <div className="project__title-card">Portfolio</div>
+
         <div className="project__card-overlay">
           <div className="project__web-links">
             {portfolioBuildLinks.map((link) => {
@@ -33,56 +26,28 @@ const Portfolio = () => {
             })}
           </div>
         </div>
-        <h4 className="project__title">Portfolio</h4>
+        <h4 className="project__title-text">Portfolio</h4>
       </div>
       {/* info */}
       <div className="project__info">
         <div className="project__info-wrapper">
-          {/* tools */}
-          <div className="project__info-tools">
-            <span className="project__info-heading">Tools</span>
-            <div className="project__tools-wrapper">
-              {portfolioBuildTools.map((tool) => {
-                const { id, name, img, url } = tool;
-                return (
-                  <div key={id} className="project__tool">
-                    <a
-                      href={url}
-                      target="_blank"
-                      rel="noreferrer"
-                      className="project__tool-link"
-                    >
-                      <img
-                        src={img}
-                        alt={name}
-                        className="project__tool-logo"
-                      />
-                    </a>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-          {/* project desc */}
-          <div className="project__desc">
-            <span className="project__info-heading">Info</span>
-            <div className="project__desc-text-wrapper">
-              <p>
-                Designed conceptually and Developed as Single Page Application
-                using React.
-              </p>
-              <p>
-                Navigation within and outside the app is setup using
-                React-Router-v6.
-              </p>
-              <p>ContextAPI is used for client state management.</p>
-              <p>Styled app with the help of Sass.</p>
-              <p>
-                Used Git for managing version control and GitHub for web hosting
-                codebase.
-              </p>
-              <p>App deployed using Netlify.</p>
-            </div>
+          <span className="project__info-heading">Info</span>
+          <div className="project__info-text-wrapper">
+            <p>
+              Designed conceptually and Developed as Single Page Application
+              using React.
+            </p>
+            <p>
+              Navigation within and outside the app is setup using
+              React-Router-v6.
+            </p>
+            <p>ContextAPI is used for client state management.</p>
+            <p>Styled app with the help of Sass.</p>
+            <p>
+              Used Git for managing version control and GitHub for web hosting
+              codebase.
+            </p>
+            <p>App deployed using Netlify.</p>
           </div>
         </div>
       </div>
