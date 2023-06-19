e09549878ba666aa83c1650e14b5df03a39b0db1
ksrinreddy <ksrinreddy@gmail.com>
2023-06-19 13:37:52 +0530

    remove images, replace with icons to fix slow loading issue

--- src/assets/data/skills.js
+++ src/assets/data/skills.js
@@ -1,76 +1,76 @@
-// langugaes images
-import htmlLogo from "../images/html-logo.png";
-import cssLogo from "../images/css-logo.png";
-import javascriptLogo from "../images/javascript-logo.png";
-import sassLogo from "../images/sass-logo.png";
-import styledComponentsLogo from "../images/styled-components-logo.png";
-// libraries images
-import reactLogo from "../images/react-logo.png";
-import reduxToolkitLogo from "../images/redux-toolkit-logo.png";
-import reactRouterLogo from "../images/react-router-logo.png";
-import reactQueryLogo from "../images/react-query-logo.png";
-// version control systems imgaes
-import gitLogo from "../images/git-logo.png";
+// react icons
+import {
+  SiHtml5,
+  SiCss3,
+  SiJavascript,
+  SiReact,
+  SiReactrouter,
+  SiRedux,
+  SiReactquery,
+  SiStyledcomponents,
+  SiGit,
+} from "react-icons/si";
+import { FaSass } from "react-icons/fa";
 
 export const skills = [
   {
     id: 1,
     name: "HTML",
-    image: htmlLogo,
+    icon: <SiHtml5 />,
     url: "https://developer.mozilla.org/en-US/docs/Web/HTML",
   },
   {
     id: 2,
     name: "CSS",
-    image: cssLogo,
+    icon: <SiCss3 />,
     url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
   },
   {
     id: 3,
     name: "JavaScript",
-    image: javascriptLogo,
+    icon: <SiJavascript />,
     url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
   },
   {
     id: 4,
     name: "ReactJS",
-    image: reactLogo,
+    icon: <SiReact />,
     url: "https://legacy.reactjs.org/",
   },
   {
     id: 5,
     name: "ReduxToolkit",
-    image: reduxToolkitLogo,
+    icon: <SiRedux />,
     url: "https://redux-toolkit.js.org/",
   },
   {
     id: 6,
     name: "React Query",
-    image: reactQueryLogo,
+    icon: <SiReactquery />,
     url: "https://tanstack.com/query/v3/",
   },
   {
     id: 7,
     name: "React Router",
-    image: reactRouterLogo,
+    icon: <SiReactrouter />,
     url: "https://reactrouter.com/en/main",
   },
   {
     id: 8,
     name: "Sass",
-    image: sassLogo,
+    icon: <FaSass />,
     url: "https://sass-lang.com/",
   },
   {
     id: 9,
     name: "Styled Components",
-    image: styledComponentsLogo,
+    icon: <SiStyledcomponents />,
     url: "https://styled-components.com/",
   },
   {
     id: 10,
     name: "Git",
-    image: gitLogo,
+    icon: <SiGit />,
     url: "https://git-scm.com/",
   },
 ];
