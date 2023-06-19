b56b9e8c69b8fbf51624a853115a80367825a9bd
ksrinreddy <ksrinreddy@gmail.com>
2023-06-19 13:42:04 +0530

    update about content and styles

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import socialLinks from "../assets/data/socialLinks";
-import portfolioPhoto from "../assets/images/portfolio-photo.jpg";
+import aboutPhoto from "../assets/images/about-photo.jpg";
 
 const About = () => {
   return (
@@ -8,16 +8,16 @@ const About = () => {
       <h2 className="about__title">about</h2>
       <div className="about__wrapper">
         {/* photo */}
-        <img src={portfolioPhoto} alt="porfile" className="about__img" />
+        <img src={aboutPhoto} alt="about" className="about__img" />
         {/* info */}
         <div className="about__info">
           <p>
-            Hello! I am Ksrin Reddy from Hyderabad, India. A Frontend Web
-            Developer with focus on React. Cloned frontend build of Netflix app,
-            designed conceptually and developed Portfolio and Resume as single
-            page applications using React. Which made me realize significance of
-            devTools during the build and debugging. Currently, looking forward
-            for an opportunity where I can apply my skills and build elegant,
+            Hello! I am Ksrin Reddy from Hyderabad, India. React focused
+            Frontend Web Developer. I have cloned frontend build of Netflix app,
+            designed conceptually and developed Portfolio as single page
+            application using React. Which made me realize the significance of
+            devTools during build and debugging. Currently, looking forward for
+            an opportunity where I can apply my skills and build elegant,
             user-friendly and efficient user interfaces.
           </p>
           <p>
